f94a7f49a8b8c0ff9b56b0d8875cd2fb3aaea7a1
David Moll <david.leander.moll@gmail.com>
2024-05-04 11:45:15 +0200

    Switched to firefox

--- _data/sites/DavidMoll.js
+++ _data/sites/DavidMoll.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: "All of my own websites",
 	options: {
 		runs: 1,
-		frequency: 60 * 23, // (in minutes)
+		frequency: 1, // (in minutes)
 	},
 	urls: [
 		"https://blog.davidmoll.net/"
